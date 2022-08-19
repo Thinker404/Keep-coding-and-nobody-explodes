23329000e6790b27c2816759bd7d41c2d7c6df12
Thinker404 <pedrixogamer2018@gmail.com>
2022-08-18 22:03:01 -0300

    PRONTO

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
-canvas.width = 900;
-canvas.height = 600;
-console.log(c);
+const MOD1 = document.getElementById('mod_1');
+const MOD2 = document.getElementById('mod_2');
+const MOD3 = document.getElementById('mod_3');
+const MOD4 = document.getElementById('mod_4');
+const canvas1 = MOD1.getContext('2d');
+const canvas2 = MOD2.getContext('2d');
+const canvas3 = MOD3.getContext('2d');
+const canvas4 = MOD4.getContext('2d');
+
+MOD1.width = 300
+MOD1.height = MOD1.width;
+
+MOD2.width = 300
+MOD2.height = MOD2.width;
+
+MOD3.width = 300
+MOD3.height = MOD3.width;
+
+MOD4.width = 300
+MOD4.height = MOD4.width;
