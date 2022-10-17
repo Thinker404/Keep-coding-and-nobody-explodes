2c32184fc2e1791b87164878e4560e0f96376048
Maths_Science <davipereiramesquitafilho@gmail.com>
2022-10-17 16:54:10 -0300

    Commitizão
    Brasil Caminhoneiro

--- script.js
+++ script.js
@@ -1,13 +1,93 @@
+
+//========================================================
+//===================INICIANDO VARIAVEIS==================
+//========================================================
+
+//mod1
+var ids = [66366336, 666366636, 10100110, 10110110];
 var seq1 = ['s1','s3','s4','s2','s5'];
 var seq2 = ['s5', 's1','s2', 's4', 's3'];
+//mod2
+var array = []; //Verificar a partir da sequencia correta
 var fio_certo = {};
-    
+
+
+//funções
+function selectID(){
+    idIndex = Math.floor(Math.random()*4)
+    id = ids[idIndex];
+    document.getElementById("ID").innerHTML = id;
+}
+function playExplosion() {
+    var audio = new Audio('/media/explosion.mp3');
+    audio.play();
+}
+
+//========================================================
+//========================================================
+//========================================================
+
+
+
+
+//========================================================
+//======Setando canvas e condicação de jogo===============
+//========================================================
+
+//criando canvas
+const MOD1 = document.getElementById('mod_1');
+const MOD2 = document.getElementById('mod_2');
+const MOD3 = document.getElementById('mod_3');
+const MOD4 = document.getElementById('mod_4');
+const canvas1 = MOD1.getContext('2d');
+const canvas2 = MOD2.getContext('2d');
+const canvas3 = MOD3.getContext('2d');
+const canvas4 = MOD4.getContext('2d');
+
+//carregando imagens do módulo 1
+var base_image1 = new Image();
+var base_image2 = new Image();
+var base_image3 = new Image();
+var base_image4 = new Image();
+
+//condigurando canvas
+MOD1.width = 300
+MOD1.height = MOD1.width;
+
+MOD2.width = 300
+MOD2.height = MOD2.width;
+
+MOD3.width = 300
+MOD3.height = MOD3.width;
+
+MOD4.width = 300
+MOD4.height = MOD4.width;
+
+//variaveis para condição de jogo
+var min = 06;
+var sec = 00;
+var condiçãoDeJogo = true;
+
+//========================================================
+//========================================================
+//========================================================
+
+
+
+
+//========================================================
+//============RELÓGIO E CONDIÇÃO DE JOGO==================
+//========================================================
+
 //Relógio da bomba
 function relogio() {
     if(condiçãoDeJogo){
 
-        if(min == 00 && sec == 00){
+        if(min == 00 && sec == 00){         //Testando se você perdeu (;
             alert("You lose!");
+            playExplosion();
+            canvas1.clearRect(0,0,400,400);
+            canvas2.clearRect(0,0,400,400);
             condiçãoDeJogo = false;
             return;
         }
@@ -39,48 +119,30 @@ function relogio() {
             return
         }
     }
-
-
-
 }
 
-
-const MOD1 = document.getElementById('mod_1');
-const MOD2 = document.getElementById('mod_2');
-const MOD3 = document.getElementById('mod_3');
-const MOD4 = document.getElementById('mod_4');
-const canvas1 = MOD1.getContext('2d');
-const canvas2 = MOD2.getContext('2d');
-const canvas3 = MOD3.getContext('2d');
-const canvas4 = MOD4.getContext('2d');
+//========================================================
+//========================================================
+//========================================================
 
 
-var base_image1 = new Image();
-var base_image2 = new Image();
-var base_image3 = new Image();
-var base_image4 = new Image();
 
 
-MOD1.width = 300
-MOD1.height = MOD1.width;
+//========================================================
+//================Animação e Desenho======================
+//========================================================
 
-MOD2.width = 300
-MOD2.height = MOD2.width;
+//Desenhando
+function criarLinha(mod2,x1,y1, x2, y2, gr, cor){
+    mod2.beginPath()
+    mod2.moveTo(x1,y1)
+    mod2.lineTo(x2,y2)
+    mod2.strokeStyle = cor
+    mod2.lineWidth = gr
+    mod2.stroke()
+}
 
-MOD3.width = 300
-MOD3.height = MOD3.width;
-
-MOD4.width = 300
-MOD4.height = MOD4.width;
-
-let min = 06;
-let sec = 00;
-
-let condiçãoDeJogo = true;
-
-var array = [];
-//ANIMATION:
-
+//funções de animação
 function pisca_verde_animation(quadro){
     let x=20;
     let y=20;
@@ -89,8 +151,6 @@ function pisca_verde_animation(quadro){
     quadro.fillStyle = '#00FF00';
     quadro.arc(x,y,radius,0,Math.PI*2);
     quadro.fill();
-    
-
 }
 function pisca_cinza_animation(quadro){
     let x=20;
@@ -100,7 +160,6 @@ function pisca_cinza_animation(quadro){
     quadro.fillStyle = '#FF0000';
     quadro.arc(x,y,radius,0,Math.PI*2);
     quadro.fill();
-
 }
 function desativado(quadro){
     let x=20;
@@ -112,12 +171,10 @@ function desativado(quadro){
     quadro.fill();
 }
 
-//CALLING
-
+//Chamando as funções
 var fps_pisca = 1;
 var tempo = 0;
 var m1, m2, m3, m4;
-
 function pisca_call(timestamp){
     setTimeout(function(){ //requestAnimationFrame to 20fps
 
@@ -140,11 +197,27 @@ function pisca_call(timestamp){
 }
 requestAnimationFrame(pisca_call);
 
-// ------------ END-ANIMATION --------------
+//========================================================
+//========================================================
+//========================================================
 
 
-//Módulo 1 - Sequência certa
 
+
+//========================================================
+//============Módulo I - sequencia de simbolos============
+//========================================================
+
+//Criando o fundo
+function criarQuadrado(mod1, x1,y1, x2, y2){
+    mod1.beginPath();
+    mod1.rect(x1,y1,x2,y2);
+    mod1.fillStyle = "#f0d45e"
+    mod1.stroke();
+    mod1.fill();   
+}
+
+//Criando quadro das imagens
 function mod1(canvas1, array){
 
     criarQuadrado(canvas1, 35,40,110,110);
@@ -164,16 +237,211 @@ function mod1(canvas1, array){
     aleatorio(4, array);
 }
 
+//Carrega as imagens nos seus respectivos lugares
+function colocarImagem(caixa, MOD1){
+    //caixa 1
+    if(caixa == 1){
+        xi1 = 35;
+        yi1 = 40;
+        xf1 = 110;
+        yf1 = 110;
+        base_image1.onload = function (e)
+        {
+            MOD1.drawImage(base_image1, xi1, yi1, xf1, yf1);
+        }
+    }
+    //caixa 2
+    if(caixa == 2){
+        xi2 = 35;
+        yi2 = 160;
+        xf2 = 110;
+        yf2 = 110;
+        base_image2.onload = function (e)
+        {
+            MOD1.drawImage(base_image2, xi2, yi2, xf2, yf2);
+        }
+    }
+    //caixa 3
+    if(caixa == 3){
+        xi3 = 160;
+        yi3 = 40;
+        xf3 = 110;
+        yf3 = 110;
+        base_image3.onload = function (e)
+        {
+            MOD1.drawImage(base_image3, xi3, yi3, xf3, yf3);
+        }
+    }
+    //caixa 4
+    if(caixa == 4){
+        xi4 = 160;
+        yi4 = 160;
+        xf4 = 110;
+        yf4 = 110;
+        base_image4.onload = function (e)
+        {
+            MOD1.drawImage(base_image4, xi4, yi4, xf4, yf4);
+        }
+    }
+}
+
+//Seleciona uma imagem aleatória
+function aleatorio(imagem, array){
+    let deixar = false;
+    //Faz um sorteio de números e coloca a imagem referente ao número enquanto todos os valores forem diferentes
+    do{
+        //Variavel de sorteio
+        var entrar = Math.floor(Math.random() * 5) + 1;
+        //Verifica se o tamanho do array(as imagens) é igual a zero(não tem imagem colocada nos quadros) então deixa a imagem sorteiada entrar;
+        if(array.length == 0){
+            deixar = true
+        }else{
+            let permitir = true;
+            //Comparar as imagens para ver se são iguais
+            for(i = 0; i < array.length; i++){
+                let verificar = array[i];
+                if(verificar == entrar){
+                    permitir = false;
+                }
+            }
+            if(permitir){
+                deixar = true
+            }
+        }
+    }while(!deixar);
+    //Verifica a qual quadro ele deve colocar a imagem
+    switch(imagem){
+        case 1:
+            base_imagem = base_image1
+            break;
+
+        case 2:
+            base_imagem = base_image2
+            break;
+
+        case 3:
+            base_imagem = base_image3
+            break;
+                    
+        case 4:
+            base_imagem = base_image4
+            break;
+    }
+
+    //Coloca a a foto no quadro 
+    switch(entrar){
+        case 1: 
+            base_imagem.src = '/media/simbolos/s1.png';
+            break;
+            
+        case 2:        
+           base_imagem.src = '/media/simbolos/s2.png';
+           break;
+
+        case 3:
+            base_imagem.src = '/media/simbolos/s3.png';
+            break;
+
+        case 4:
+            base_imagem.src = '/media/simbolos/s4.png';
+            break;
+
+        case 5:
+            base_imagem.src = '/media/simbolos/s5.png';
+            break;
+
+        case 6:
+            base_imagem.src = '/media/simbolos/s6.png';
+            break;
+        
+        case 7:
+            base_imagem.src = '/media/simbolos/s7.png';
+            break;
+        
+        case 8:
+            base_imagem.src = '/media/simbolos/s8.png';
+            break;
+        
+        case 9:
+            base_imagem.src = '/media/simbolos/s9.png';
+            break;
+        
+        case 10:
+            base_imagem.src = '/media/simbolos/s10.png';
+            break;
+            
+    }
+    if(deixar){
+        array.push(`${entrar}`);
+    }
+}
+
+//Ouvindo os cliques
+MOD1.addEventListener('click', (event) => {
+
+    var mouse_y = event.offsetY;
+    var mouse_x = event.offsetX;
+    if(((mouse_y >= 40) && (mouse_y <= 150)) && ((mouse_x >=35) && (mouse_x <= 145))){
+        sequencia('s1')
+    }
+    if(((mouse_y >= 40) && (mouse_y <= 150)) && ((mouse_x >=160) && (mouse_x <= 270))){
+        sequencia('s2')
+    }
+    if(((mouse_y >= 160) && (mouse_y <= 270)) && ((mouse_x >=35) && (mouse_x <= 145))){
+        sequencia('s3')
+    }
+    if(((mouse_y >= 160) && (mouse_y <= 270)) && ((mouse_x >=160) && (mouse_x <= 270))){
+        sequencia('s4')
+    }
+
+})
+
+var aperto = 0;
+function sequencia(clicado){
+
+    /*
+    1 - Comparar o array com o .json
+    2 - Colocar a ordem correta
+    3 - Verificar se a ordem esta correta
+    4 - Chorar pq isso n vai ser fácil
+    */ 
+    var certo = seq1[aperto]
+    if(clicado == certo){
+        aperto+=1 
+        
+        if(aperto == 4){
+            m1 = true;
+            desativado(canvas1)
+        }
+
+    }else{
+        aperto = 0;
+        alert('errou')
+        min = 0;
+        sec = 0;
+    }
+}
+
+//========================================================
+//========================================================
+//========================================================
 
 
-//Modulo 2 - Corte dos fios
+
+
+//========================================================
+//==============Módulo II - Corte dos fios/===============
+//========================================================
+
 function mod2(canvas2){
 
+    //Definindo valor das cores e
     let amarelo = "#ffff66";
     let rosa = "#ff80c1";
     let azul = "#99ffdd";
     let branco = "#ffffff";
     let verde = "#66ff99";
+    //Número de fios:
     var nf_amarelo = 0;
     var nf_rosa = 0;
     var nf_azul = 0;
@@ -196,6 +464,7 @@ function mod2(canvas2){
                 counter++;
             }
         };
+
         //contando fios
         switch(target){
         case "#ffff66":
@@ -219,7 +488,7 @@ function mod2(canvas2){
         }
     }
     
-    //FIO CERTO PARA CORTAR
+    //Definindo qual fio cortar
     switch(nf_branco){
         case 0:
             if(cor[2] == "#66ff99"){
@@ -262,9 +531,7 @@ function mod2(canvas2){
             break;
     }
 
-    console.log(nf_amarelo, nf_rosa, nf_azul, nf_branco, nf_verde);
-    console.log(fio_certo.num);
-
+    //desenhando fios
     criarLinha(canvas2, 0, 50, 300, 50, 7, cor[0]);
     criarLinha(canvas2, 0, 100, 300, 100, 7, cor[1]);
     criarLinha(canvas2, 0, 150, 300, 150, 7, cor[2]);
@@ -272,236 +539,22 @@ function mod2(canvas2){
     criarLinha(canvas2, 0, 250, 300, 250, 7, cor[4]);
 }
 
-
-
+//Checando o que fazer com o fio cortado
 function fio_cortado(fio){
     if(fio == fio_certo.num){
+        m2 = true;
         return alert("mestre dos fios")
+        
     }else{
         console.log(fio)
         return alert("kabum katau")
+        min = 0;
+        sec = 0;
     }
 }
 
-
-
-
-
-setInterval(relogio, 1000); 
-mod1(canvas1, array);
-mod2(canvas2);
-
-/*<------------------- Funcionamento dos módulos ------------------->*/ 
-
-
-//Módulo 1 
-
-//Cria o fundo
-function criarQuadrado(mod1, x1,y1, x2, y2){
-    mod1.beginPath();
-    mod1.rect(x1,y1,x2,y2);
-    mod1.fillStyle = "#f0d45e"
-    mod1.stroke();
-    mod1.fill();   
-}
-
-
-//Carrega as imagens nos seus respectivos lugares
-function colocarImagem(caixa, MOD1){
-
-    if(caixa == 1){
-        xi1 = 35;
-        yi1 = 40;
-        xf1 = 110;
-        yf1 = 110;
-
-        base_image1.onload = function (e)
-        {
-            MOD1.drawImage(base_image1, xi1, yi1, xf1, yf1);
-        }
-    }
-
-    if(caixa == 2){
-        xi2 = 35;
-        yi2 = 160;
-        xf2 = 110;
-        yf2 = 110;
-
-        base_image2.onload = function (e)
-        {
-            MOD1.drawImage(base_image2, xi2, yi2, xf2, yf2);
-        }
-    }
-
-    if(caixa == 3){
-        xi3 = 160;
-        yi3 = 40;
-        xf3 = 110;
-        yf3 = 110;
-
-        base_image3.onload = function (e)
-        {
-            MOD1.drawImage(base_image3, xi3, yi3, xf3, yf3);
-        }
-    }
-
-    if(caixa == 4){
-        xi4 = 160;
-        yi4 = 160;
-        xf4 = 110;
-        yf4 = 110;
-
-        base_image4.onload = function (e)
-        {
-            MOD1.drawImage(base_image4, xi4, yi4, xf4, yf4);
-        }
-    }
-}
-
-
-//Seleciona uma imagem aleatória
-
-/*
-Caso a imagem seja igual ele deverá escolher outra diferente
-*/
-function aleatorio(imagem, array){
-    let deixar = false;
-
-    //Faz um sorteio de números e coloca a imagem referente ao número enquanto todos os valores forem diferentes
-    do{
-        //Variavel de sorteio
-        var entrar = Math.floor(Math.random() * 5) + 1;
-
-        //Verifica se o tamanho do array(as imagens) é igual a zero(não tem imagem colocada nos quadros) então deixa a imagem sorteiada entrar;
-        if(array.length == 0){
-            deixar = true
-
-        }else{
-
-            let permitir = true;
-            
-            //Comparar as imagens para ver se são iguais
-            for(i = 0; i < array.length; i++){
-            
-                let verificar = array[i];
-                if(verificar == entrar){
-                    permitir = false;
-                }
-            }
-
-            if(permitir){
-                deixar = true
-            }
-        }
-    }while(!deixar);
-
-    //Verifica a qual quadro ele deve colocar a imagem
-    switch(imagem){
-        case 1:
-            base_imagem = base_image1
-            break;
-
-        case 2:
-            base_imagem = base_image2
-            break;
-
-        case 3:
-            base_imagem = base_image3
-            break;
-                    
-        case 4:
-            base_imagem = base_image4
-            break;
-    }
-
-    //Coloca a a foto no quadro 
-    switch(entrar){
-        case 1: 
-            base_imagem.src = '/media/simbolos/s1.png';
-            break;
-            
-        case 2:        
-           base_imagem.src = '/media/simbolos/s2.png';
-           break;
-
-        case 3:
-            base_imagem.src = '/media/simbolos/s3.png';
-            break;
-
-        case 4:
-            base_imagem.src = '/media/simbolos/s4.png';
-            break;
-
-        case 5:
-            base_imagem.src = '/media/simbolos/s5.png';
-            break;
-    }
-    
-
-    if(deixar){
-        array.push(`${entrar}`);
-    }
-}
-
-MOD1.addEventListener('click', (event) => {
-
-    var mouse_y = event.offsetY;
-    var mouse_x = event.offsetX;
-    if(((mouse_y >= 40) && (mouse_y <= 150)) && ((mouse_x >=35) && (mouse_x <= 145))){
-        sequencia('s1')
-    }
-    if(((mouse_y >= 40) && (mouse_y <= 150)) && ((mouse_x >=160) && (mouse_x <= 270))){
-        sequencia('s2')
-    }
-    if(((mouse_y >= 160) && (mouse_y <= 270)) && ((mouse_x >=35) && (mouse_x <= 145))){
-        sequencia('s3')
-    }
-    if(((mouse_y >= 160) && (mouse_y <= 270)) && ((mouse_x >=160) && (mouse_x <= 270))){
-        sequencia('s4')
-    }
-
-})
-
-var aperto = 0;
-function sequencia(clicado){
-
-    /*
-    1 - Comparar o array com o .json
-    2 - Colocar a ordem correta
-    3 - Verificar se a ordem esta correta
-    4 - Chorar pq isso n vai ser fácil
-    */ 
-    var certo = seq1[aperto]
-    if(clicado == certo){
-        aperto+=1 
-        console.log('Errou!')
-        
-        if(aperto == 4){
-            m1 = true;
-            desativado(canvas1)
-        }
-
-    }
-    else{
-        aperto = 0;
-        alert('errou')
-    }
-}
-
-
-//Modulo 2
-
-function criarLinha(mod2,x1,y1, x2, y2, gr, cor){
-    mod2.beginPath()
-    mod2.moveTo(x1,y1)
-    mod2.lineTo(x2,y2)
-    mod2.strokeStyle = cor
-    mod2.lineWidth = gr
-    mod2.stroke()
-}
-
+//Checando se o fio foi cortado
 function cortar(index_fio){
-    
     switch (index_fio){
         case 0:
             xi = 10;
@@ -510,7 +563,6 @@ function cortar(index_fio){
             yf = 15;
             fio_cortado(0)
             break;
-            
         case 1:
             xi = 10;
             yi = 95;
@@ -518,7 +570,6 @@ function cortar(index_fio){
             yf = 15;
             fio_cortado(1)
             break;
-            
         case 2:
             xi = 10;
             yi = 145;
@@ -526,7 +577,6 @@ function cortar(index_fio){
             yf = 15;
             fio_cortado(2)
             break;
-
         case 3:
             xi = 10;
             yi = 195;
@@ -534,7 +584,6 @@ function cortar(index_fio){
             yf = 15;
             fio_cortado(3)
             break;
-
         case 4:
             xi = 10;
             yi = 245;
@@ -543,17 +592,11 @@ function cortar(index_fio){
             fio_cortado(4)
             break;
     }   
-
     canvas2.clearRect(xi,yi,xf,yf);
-
 }
 
+//Ouvindo clicos
 MOD2.addEventListener('click', (event) => {
-
-    //const rect = MOD2.getBoundingClientRect();
-    //console.log(event)
-    //var mouse_x = event.offsetX;
-
     var mouse_y = event.offsetY;
     if((mouse_y >= 50-4) && (mouse_y <= 57-4)){
         cortar(0);
@@ -571,6 +614,36 @@ MOD2.addEventListener('click', (event) => {
     if((mouse_y >= 250-4) && (mouse_y <= 257-4)){
         cortar(4);
     }
-
 })
 
+//========================================================
+//========================================================
+//========================================================
+
+
+
+
+//========================================================
+//===========Módulo III - Botão de apertar===============
+//========================================================
+
+
+//========================================================
+//========================================================
+//========================================================
+
+
+
+
+//========================================================
+//===================CALLING EVERYTHING===================
+//========================================================
+
+setInterval(relogio, 1000); 
+mod1(canvas1, array);
+mod2(canvas2);
+selectID();
+
+//========================================================
+//========================================================
+//========================================================
